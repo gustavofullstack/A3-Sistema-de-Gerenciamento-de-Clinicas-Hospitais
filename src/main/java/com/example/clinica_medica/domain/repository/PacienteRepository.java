e9119c79a5e98f2751a4e0f1e5a1c33d358cf8e8
João Vinicius <joaodessimonifernandes@outlook.com>
2024-11-15 13:51:23 -0300

    chore: migração para mysql

--- src/main/java/com/example/clinica_medica/domain/repository/PacienteRepository.java
+++ src/main/java/com/example/clinica_medica/domain/repository/PacienteRepository.java
@@ -1,13 +1,7 @@
 package com.example.clinica_medica.domain.repository;
 
 import com.example.clinica_medica.domain.model.Paciente;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Date;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
